df8e58567d0c2c67471293d3db2048368e925a07
HRusby <harrygeorger@hotmail.com>
2021-05-02 11:53:59 +0100

    Adding initial fields to Metric

---
+++ Database/DML/InitialMetrics.sql
@@ -0,0 +1,4 @@
+INSERT INTO METRIC (Name, ShortName) VALUES ('Gram', 'g');
+INSERT INTO METRIC (Name, ShortName) VALUES ('Kilogram', 'kg');
+INSERT INTO METRIC (Name, ShortName) VALUES ('Millilitre', 'ml');
+INSERT INTO METRIC (Name, ShortName) VALUES ('Litre', 'l');
