f8531c0f012d26f517e4a341c603098e5b335daa
HRusby <harrygeorger@hotmail.com>
2021-05-29 12:39:32 +0100

    Adding user to vuex state

--- calorie-counting/src/store/index.js
+++ calorie-counting/src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: -1
   },
   mutations: {
+    setUser(id){
+      this.state.user = id
+    }
   },
   actions: {
   },
