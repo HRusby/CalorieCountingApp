40d9b110f4ee7df89d838cc167f4878e11c2fc7b
HRusby <harrygeorger@hotmail.com>
2021-05-29 11:13:34 +0100

    Setting up .vue files

--- calorie-counting/src/components/ModeSelection.vue
+++ calorie-counting/src/components/ModeSelection.vue
@@ -4,6 +4,13 @@
 
 <script>
 export default {
+  
+}
+</script>
+
+
+<script>
+export default {
   name: 'ModeSelection',
   props: {
     user: {
@@ -15,18 +22,4 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
