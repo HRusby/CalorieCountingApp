3482ee90d517ff2dee1538d12109ce1fb81c9cdd
HRusby <harrygeorger@hotmail.com>
2021-05-29 12:39:49 +0100

    Removing Hello World from app

--- calorie-counting/src/App.vue
+++ calorie-counting/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <user-selection></user-selection>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserSelection from './components/UserSelection.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserSelection
   }
 }
 </script>
