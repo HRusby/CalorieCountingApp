54b21991e51159fb9c8625cb47b2b75c2efe7f0b
HRusby <harrygeorger@hotmail.com>
2021-05-29 12:39:19 +0100

    Implementing simple user selection view

--- calorie-counting/src/components/UserSelection.vue
+++ calorie-counting/src/components/UserSelection.vue
@@ -1,13 +1,33 @@
 <template>
-    <div></div>    
+  <div>
+    <p v-for="user in users" :key="user.id" @click="setUser(user.id)">
+      {{ user.name }}
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  name: "HelloWorld",
+  props: {},
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+      setUser(id){
+          console.log(id)
+      }
+  },
+  created(){
+      this.users = [
+          {name: "harry", id: 1},
+          {name: "max", id: 2}
+      ]
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
