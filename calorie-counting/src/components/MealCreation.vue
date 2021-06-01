633ab064df6853dacf6bbc161aa7ba1be67ceb74
HRusby <harrygeorger@hotmail.com>
2021-06-01 11:47:51 +0100

    Adding simple logic to meal creation

--- calorie-counting/src/components/MealCreation.vue
+++ calorie-counting/src/components/MealCreation.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>Meal Creation</div>
+  <div class="flex space-x-4 grid grid-cols-1">
+    <h1 class="mb-2">Meal Creation</h1>
+    <br />
+    <button
+      href="#"
+      v-if="mode !== ''"
+      @click="mode = ''"
+      class="rounded-full h-20 w-40 flex items-center justify-center bg-green-600 text-lg text-white mx-auto"
+    >
+      Change Mode
+    </button>
+    <div v-else>
+      <button
+        href="#"
+        @click="mode = 'addMeal'"
+        class="rounded-full h-20 w-40 flex items-center justify-center bg-green-600 text-lg text-white mx-auto"
+      >
+        Add Meal
+      </button>
+      <button
+        href="#"
+        @click="mode = 'updateMeal'"
+        class="rounded-full h-20 w-40 flex items-center justify-center bg-green-600 text-lg text-white mx-auto"
+      >
+        Update Meal
+      </button>
+    </div>
+    <p>Mode = {{ mode }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  name: "MealCreation",
+  data() {
+    return {
+      mode: "",
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
