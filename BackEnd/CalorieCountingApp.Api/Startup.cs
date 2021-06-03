62a145cdbff8230eaafe085859c34ac2b90847d0
HRusby <harrygeorger@hotmail.com>
2021-06-03 13:48:54 +0100

    Adding docker-compose port

--- BackEnd/CalorieCountingApp.Api/Startup.cs
+++ BackEnd/CalorieCountingApp.Api/Startup.cs
@@ -43,7 +43,7 @@ namespace CalorieCountingApp.Api
 
             app.UseCors(builder =>
                     builder
-                        .WithOrigins("http://localhost:3000", "http://localhost:7001", "http://localhost:8080")
+                        .WithOrigins("http://localhost:3000", "http://localhost:7001", "http://localhost:8080", "http://localhost:8001")
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 
