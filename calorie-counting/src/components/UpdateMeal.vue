158351f65fe260ec4b4b118fd7d7c9021a5989fe
HRusby <harrygeorger@hotmail.com>
2021-06-03 18:55:43 +0100

    Adding Changing display to form to allow updating meal data

--- calorie-counting/src/components/UpdateMeal.vue
+++ calorie-counting/src/components/UpdateMeal.vue
@@ -1,50 +1,61 @@
 <template>
   <div>
     <h1>Update Meal</h1>
-    <p>{{ meal.id }}</p>
-    <p>{{ meal.name }}</p>
-    <p>{{meal.cookedWeight}}</p>
-    <metric-select :modelValue="meal.cookedWeightMetricId" @update:modelValue="(val) => {updateMeal('cookedWeightMetricId', val)}" ></metric-select>
-    <p>{{meal.remainingWeight}}</p>
-    <p>{{meal.cookedOn}}</p>
+    <form @submit.prevent="submitForm">
+      <input type="text" title="Name" v-model="meal.name" />
+      <input
+        type="number"
+        title="Cooked Weight"
+        min="0"
+        step="0.01"
+        v-model="meal.cookedWeight"
+      />
+      <metric-select
+        title="Weight Metric"
+        v-model="meal.cookedWeightMetricId"
+      />
+      <input
+        type="number"
+        title="Remaining Weight"
+        min="0"
+        step="0.01"
+        v-model="meal.remainingWeight"
+      />
+      <input type="date" title="Cooked On" v-model="meal.cookedOn" />
+      <button type="submit">Submit Changes</button>
+    </form>
   </div>
 </template>
 
 <script>
 import ConfigData from "../config/config.json";
-import MetricSelect from './MetricSelect.vue';
+import MetricSelect from "./MetricSelect.vue";
 export default {
   name: "UpdateMeal",
   components: { MetricSelect },
   props: {
     mealToUpdate: {
       required: true,
-      type: Object
-    }
+      type: Object,
+    },
+  },
+  data() {
+    return { meal: this.mealToUpdate };
   },
   methods: {
-    updateMeal(propertyName, newValue){
-      this.meal[propertyName] = newValue
-      this.$emit('updatedMeal', this.meal)
-      this.pushToApi()
-    },
-    pushToApi(){
+    submitForm() {
       fetch(ConfigData.backendUrl + "Meal/UpdateMeal", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(this.meal)
+        body: JSON.stringify(this.meal),
       })
-      .then((resp) => resp.json())
-      .then((data) => console.log('data: '+data));
-    }
+        .then((resp) => resp.json())
+        .then((data) => console.log("data: " + data));
+      this.$emit("updatedMeal", this.meal);
+    },
   },
-  computed: {
-    meal() {
-      return this.mealToUpdate
-    }
-  }
 };
 </script>
 
