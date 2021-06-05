55fc26cbbe0fccd4c8b44ece09f4b539f5a3c578
HRusby <harrygeorger@hotmail.com>
2021-06-05 18:10:27 +0100

    Adding API call and form to AddIndividualIngredientRecord

--- calorie-counting/src/components/Tracking/AddIndividualIngredientRecord.vue
+++ calorie-counting/src/components/Tracking/AddIndividualIngredientRecord.vue
@@ -1,24 +1,49 @@
 <template>
     <div>
         <h1>Add Individual Ingredient Record</h1>
-        <form @submit.prevent="">
-
+        <form @submit.prevent="submitForm">
+            <ingredient-select v-model="mealOrIngredientId" />
+            <input type="number" min="0.0" step="0.01" v-model="quantity" title="quantity" placeholder="quantity" />
+            <input type="datetime" v-model="dateTime" title="Date Time" placeholder="datetime" />
+            <button type="submit" v-text="'Submit'"/>
         </form>
     </div>
 </template>
 
 <script>
+import ConfigData from '../../config/config.json';
+import IngredientSelect from '../Ingredient/IngredientSelect.vue'
 export default {
+  components: { IngredientSelect },
     name: "AddIndividualIngredientRecord",
     data(){
         return {
-
+            mealOrIngredientId: NaN,
+            quantity: NaN,
+            dateTime: new Date().toISOString()
         }
     },
     methods:{
         submitForm(){
-            let record = {}
-            this.$emit('addRecord', record)
+            let record = {
+                mealOrIngredientId: this.mealOrIngredientId,
+                userId: this.$store.getters.selectedUser,
+                typeId: 1,
+                quantity: this.quantity,
+                dateTime: this.dateTime
+            }
+
+            fetch(ConfigData.backendUrl + "Tracking/AddNewRecord", {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify(record),
+            })
+            .then((resp) => resp.json())
+            .then((data) => console.log("data: " + data));
+
+            //this.$emit('addRecord', record)
         }
     }
 }
