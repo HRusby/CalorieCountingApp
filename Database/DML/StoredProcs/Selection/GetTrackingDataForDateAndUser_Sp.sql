872087d97d66cbd92a869ff1a2dd325802b32ab7
HRusby <harrygeorger@hotmail.com>
2021-06-05 16:26:27 +0100

    Updating GetTrackingDataForDateAndUser to retrieve from both Sub-Tracking tables and union all the results

--- Database/DML/StoredProcs/Selection/GetTrackingDataForDateAndUser_Sp.sql
+++ Database/DML/StoredProcs/Selection/GetTrackingDataForDateAndUser_Sp.sql
@@ -1,20 +1,42 @@
 CREATE PROCEDURE GetTrackingDataForDateAndUser(
-    IN $UserId INTEGER,
-    IN $Date DATE
-)
+	IN $UserId INTEGER, 
+	IN $Date DATE) 
 BEGIN
-    SELECT 
-	    t.Id,
-	    t.UserId,
-		t.IngredientId,
-	    t.MetricId,
-	    t.Quantity,
-        t.`DateTime`,
-		i.Name IngredientName,
-	    m.ShortName MetricShortName
+	SELECT t.Id TrackingId,
+		mt.Id SubTrackingId,
+		t.UserId,
+		tt.Id TypeId,
+		tt.`Type` Type,
+		m.Name SubTrackingName,
+		t.Quantity,
+		t.Calories,
+		m2.ShortName,
+		t.DateTime
 	FROM TRACKING t
-	INNER JOIN METRIC m ON t.MetricId = m.Id
-	INNER JOIN INGREDIENT i ON t.IngredientId = i.Id 
-	WHERE UserId = $UserId
-    AND DATE(`DateTime`) = $Date ;
+		INNER JOIN MEAL_TRACKING mt ON t.Id = mt.TrackingId
+		INNER JOIN MEAL m ON mt.MealId = m.Id
+		INNER JOIN TRACKING_TYPE tt ON t.TrackingTypeId = tt.Id
+		INNER JOIN METRIC m2 ON m.CookedWeightMetricId = m2.Id
+	WHERE t.UserId = 1
+		AND t.TrackingTypeId = 1
+		AND DATE(t.`DateTime`) = $Date
+	UNION ALL
+	SELECT t.Id,
+		it.Id SubTrackingId,
+		t.UserId,
+		tt.Id TypeId,
+		tt.`Type` Type,
+		i.Name SubTrackingName,
+		t.Quantity,
+		t.Calories,
+		m.ShortName,
+		t.DateTime
+	FROM TRACKING t
+		INNER JOIN INDIVIDUAL_TRACKING it ON t.Id = it.TrackingId
+		INNER JOIN INGREDIENT i ON it.IngredientId = i.Id
+		INNER JOIN TRACKING_TYPE tt ON t.TrackingTypeId = tt.Id
+		INNER JOIN METRIC m ON i.MetricId = m.Id
+	WHERE t.UserId = 1
+		AND t.TrackingTypeId = 2
+		AND DATE(t.`DateTime`) = $Date;
 END;
