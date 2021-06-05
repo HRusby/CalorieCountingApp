576b50dd30c0c7c52727bf8777ded23935522780
HRusby <harrygeorger@hotmail.com>
2021-06-05 13:27:45 +0100

    Displaying User Tracking Records for a date

--- calorie-counting/src/components/CalorieTracking.vue
+++ calorie-counting/src/components/CalorieTracking.vue
@@ -1,10 +1,44 @@
 <template>
-    <div>Calorie Tracking</div>
+    <div>
+        <h1>Calorie Tracking</h1>
+        <input type="date" title="Tracking Date" :value="trackingDate" />
+        <tracking-record v-for="trackingRecord in trackingRecords" :record="trackingRecord" :key="trackingRecord.id" />
+    </div>
 </template>
 
 <script>
+import ConfigData from "../config/config.json";
+import TrackingRecord from './Tracking/TrackingRecord.vue';
 export default {
-    name: "CalorieTracking"
+  components: { TrackingRecord },
+    name: "CalorieTracking",
+    data(){
+        return {
+            trackingDate: new Date().toISOString().substr(0,10),
+            trackingRecords: []
+        }
+    },
+    methods: {
+        getTrackingData(){
+            // Fetch tracking data from api for the given date and user
+            let trackingRequest = {
+                userId: this.$store.getters.selectedUser,
+                date: this.trackingDate
+            }
+            fetch(ConfigData.backendUrl + "Tracking/GetTrackingDataForDateAndUser", {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify(trackingRequest),
+            })
+            .then(resp => resp.json())
+            .then(data => this.trackingRecords = data);
+        }
+    },
+    created(){
+        this.getTrackingData()
+    }
 }
 </script>
 
---
+++ calorie-counting/src/components/Tracking/TrackingRecord.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{record.ingredientName}} - {{record.quantity}}{{record.metricShortName}} {{record.dateTime}}
+    </div>
+</template>
+
+<script>
+
+/** record has properties:
+ *  id, userId, ingredientId, metricId, quantity, dateTime, ingredientName, metricShortName
+ */
+export default {
+    name: "TrackingRecord",
+    props: {
+        record: {
+            required: true,
+            type: Object
+        }
+    }
+}
+</script>
